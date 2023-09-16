abdcf6cf69171de4f7ca500633a21b482a94820d
Kevin Lambert <939866+kevinlambert@users.noreply.github.com>
2023-09-16 13:25:18 +0200

    fix screen orientation resize

--- src/util/viewport.js
+++ src/util/viewport.js
@@ -1,6 +1,10 @@
 const documentHeight = () => {
-  const doc = document.documentElement;
-  doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+  // timeout fixes race condition when mobile screen orientation changes
+  setTimeout(() => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+  }, 20);
 };
+// this is a fallback when dvh css element is not valid
 window.addEventListener("resize", documentHeight);
 documentHeight();
