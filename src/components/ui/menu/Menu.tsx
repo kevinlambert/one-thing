ff0df8c6ed14ac7b949d5614902861f60afb8db0
Kevin Lambert <939866+kevinlambert@users.noreply.github.com>
2023-10-14 22:36:54 +0200

    hide menu items

--- src/components/ui/menu/Menu.tsx
+++ src/components/ui/menu/Menu.tsx
@@ -41,12 +41,12 @@ export default ({ isMenuOpen, onCloseHandler }: Props) => {
         <div className="menu-items">
           <NavLink to={routePathHelper.tour({ step: 0 })}>View Tour</NavLink>
         </div>
-        <div className="menu-items">
+        {/* <div className="menu-items">
           <NavLink to={routePaths.PLANS}>Plans</NavLink>
         </div>
         <div className="menu-items">
           <NavLink to={routePaths.MANAGEBILLING}>Manage billing</NavLink>
-        </div>
+        </div> */}
         <div className="menu-items">
           <a href="mailto:onething@lambert.com.co?subject=Feedback on ONE Thing App&body=">
             Feedback
